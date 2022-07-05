ab1eb042fc61a74a1164f13a7c234c33d7c70803
Eduard Mielieshkin <proxaker@workmail.com>
2022-07-05 08:25:37 +0000

    [MR-1] Moved functions to 'private' and changed 'val' to 'var'

--- src/main/scala/org/apache/spark/metrics/StageMetrics.scala
+++ src/main/scala/org/apache/spark/metrics/StageMetrics.scala
@@ -11,33 +11,33 @@ import org.apache.spark.internal.Logging
 
 import com.codahale.metrics.{Gauge, MetricRegistry}
 
-class StageMetrics extends DriverPlugin with Logging {
+private[spark] class StageMetrics extends DriverPlugin with Logging {
   private var sparkContext: SparkContext = _
   private var pluginContext: PluginContext = _
 
-  private val metricStagePrefix: String = "stage"
+  private var metricStagePrefix: String = "stage"
 
-  def get_stageStatus(metricStage: StageData): Int = {
-    val currentStage: StageData = sparkContext.statusStore.stageList(null).head
+  private def get_stageStatus(metricStage: StageData): Int = {
+    var currentStage: StageData = sparkContext.statusStore.stageList(null).head
     if(metricStage.stageId == currentStage.stageId) return 1
     else return 0
   }
 
-  def get_numTasks(metricStage: StageData): Int = {
+  private def get_numTasks(metricStage: StageData): Int = {
     return metricStage.numTasks
   }
 
-  def get_numActiveTasks(metricStage: StageData): Int = {
+  private def get_numActiveTasks(metricStage: StageData): Int = {
     return metricStage.numActiveTasks
   }
 
-  def get_numCompleteTasks(metricStage: StageData): Int = {
+  private def get_numCompleteTasks(metricStage: StageData): Int = {
     return metricStage.numCompleteTasks
   }
 
   // numTasks, numActiveTasks, numCompleteTasks
-  def get_metric_value(metricName: String, metricStageId: Int): Int = {
-    val metricStage: StageData = sparkContext.statusStore.stageList(null).find(_.stageId == metricStageId).head
+  private def get_metric_value(metricName: String, metricStageId: Int): Int = {
+    var metricStage: StageData = sparkContext.statusStore.stageList(null).find(_.stageId == metricStageId).head
     return metricName match {
       case "status" => get_stageStatus(metricStage)
       case "numTasks" => get_numTasks(metricStage)
@@ -49,23 +49,23 @@ class StageMetrics extends DriverPlugin with Logging {
     }
   }
 
-  def is_metric_registred(metricStageId: Int, metricName: String): Boolean = {
-    val metricRegistryName: String = s"$metricStagePrefix.${metricStageId.toString}.$metricName"
+  private def is_metric_registred(metricStageId: Int, metricName: String): Boolean = {
+    var metricRegistryName: String = s"$metricStagePrefix.${metricStageId.toString}.$metricName"
     return pluginContext.metricRegistry.getNames().contains(metricRegistryName)
   }
 
   // TODO: can be many active stages
-  def registerMetrics(): Int = {
-    val metricNames: Seq[String] = Seq("status", "numTasks", "numActiveTasks", "numCompleteTasks")
-    val metricRegistry = pluginContext.metricRegistry
-    val currentStageId: Int = sparkContext.statusStore.stageList(null).head.stageId
+  private def registerMetrics(): Int = {
+    var metricNames: Seq[String] = Seq("status", "numTasks", "numActiveTasks", "numCompleteTasks")
+    var metricRegistry = pluginContext.metricRegistry
+    var currentStageId: Int = sparkContext.statusStore.stageList(null).head.stageId
 
     metricNames.foreach(x =>
       if(!is_metric_registred(currentStageId, x)) {
         metricRegistry.register(MetricRegistry.name(metricStagePrefix, currentStageId.toString, x), new Gauge[Int] {
           override def getValue: Int = {
-            val metricName = x
-            val metricStageId = currentStageId
+            var metricName = x
+            var metricStageId = currentStageId
             get_metric_value(metricName, currentStageId)
           }
         })
@@ -74,7 +74,7 @@ class StageMetrics extends DriverPlugin with Logging {
   }
 
   private[spark] override def init(sc: SparkContext, pc: PluginContext): JMap[String, String] = {
-    val metricRegistry = pc.metricRegistry
+    var metricRegistry = pc.metricRegistry
     //val sourceName = "DAGScheduler"
 
     sparkContext = sc
